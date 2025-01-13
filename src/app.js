19a49780a62c46806b364eb895f203f047b04083
kbChandan <emmichandan333@gmail.com>
2025-01-13 17:20:17 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.use("/deepi", (req, res) => {
+  res.send("How are u deepu rani");
+});
+app.listen(4000, () => {
+  console.log("Server is up and running succesfully");
+});
