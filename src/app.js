5dd913cb891dcc9a33a456ceb48372e208263d33
kbChandan <emmichandan333@gmail.com>
2025-01-13 17:35:42 +0530

    added console

--- src/app.js
+++ src/app.js
@@ -4,5 +4,6 @@ app.use("/deepi", (req, res) => {
   res.send("How are u deepu rani");
 });
 app.listen(4000, () => {
+  console.log("Im king");
   console.log("Server is up and running succesfully");
 });
