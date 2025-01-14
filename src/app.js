a5e57614ab5d476d3fdf53ef22d6c03a20840fd9
kbChandan <emmichandan333@gmail.com>
2025-01-14 17:35:38 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const app = express();
+
+// This will only handle api get call method
+app.get("/user", (req, res) => {
+  res.send("retrieved data successfully");
+});
+
+app.post("/user", (req, res) => {
+  res.send("sent successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted successfully");
+});
+
+// This will match all the api call method
+app.use("/user", (req, res) => {
+  res.send("user data successfully");
+});
 app.use("/deepi", (req, res) => {
   res.send("How are u deepu rani");
 });
